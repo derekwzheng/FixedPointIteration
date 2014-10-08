5b2a336e53d4d3880c1c43ad8d9c821cf05b5d30
derekwzheng <derekwzheng@gmail.com>
2014-10-08 16:53:24 -0700

    Add documentation of the function fixedPointIteration()

---
+++ fixPointIteration.py
@@ -0,0 +1,5 @@
+"""
+The function fixedPointIteration() computes approximate solution of g(x) = x.
+It takes a single-variable function G, starting guess X_0, and number of
+iteration steps K, and returns an approximate solution. 
+"""
